cd63b3865ee95f22202b7313cb4b654dae997977
KainSlow <jorge_alfonso3@hotmail.com>
2025-04-16 20:07:31 -0400

    use static function instead of instanced method

--- Aplicacion.java
+++ Aplicacion.java
@@ -2,23 +2,22 @@ import clases.Algoritmos;
 
 public class Aplicacion{
     public static void main(String[]args){
-        Algoritmos algo = new Algoritmos();
 
-        System.out.println("10 es par? " + algo.esPar(10));
-        System.out.println("2 es par? " + algo.esPar(2));
-        System.out.println("19 es par? " + algo.esPar(19));
+        System.out.println("10 es par? " + Algoritmos.esPar(10));
+        System.out.println("2 es par? " + Algoritmos.esPar(2));
+        System.out.println("19 es par? " + Algoritmos.esPar(19));
 
-        System.out.println("17 es primo? " + algo.esPrimo(17));
-        System.out.println("11 es primo? " + algo.esPrimo(11));
-        System.out.println("100 es primo? " + algo.esPrimo(100));
+        System.out.println("17 es primo? " + Algoritmos.esPrimo(17));
+        System.out.println("11 es primo? " + Algoritmos.esPrimo(11));
+        System.out.println("100 es primo? " + Algoritmos.esPrimo(100));
 
-        System.out.println(algo.stringEnReversa("severlA"));
-        System.out.println(algo.stringEnReversa("Hola!"));
+        System.out.println(Algoritmos.stringEnReversa("severlA"));
+        System.out.println(Algoritmos.stringEnReversa("Hola!"));
 
-        System.out.println(algo.esPalindromo("rapar"));
-        System.out.println(algo.esPalindromo("pan"));
-        System.out.println(algo.esPalindromo("La tomo como tal"));
+        System.out.println(Algoritmos.esPalindromo("rapar"));
+        System.out.println(Algoritmos.esPalindromo("pan"));
+        System.out.println(Algoritmos.esPalindromo("La tomo como tal"));
 
-        algo.secuenciaFizzBuzz(20);
+        Algoritmos.secuenciaFizzBuzz(20);
     }
 }
