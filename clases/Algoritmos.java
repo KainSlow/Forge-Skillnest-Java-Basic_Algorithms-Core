70e6a7646df70a51ecdcdc19a471d18e7503196e
KainSlow <jorge_alfonso3@hotmail.com>
2025-04-20 15:39:59 -0400

    Updated: now FizzBuzz properly adds "FizzBuzz" to the secuence when the number is divisible by 3 and 5

--- clases/Algoritmos.java
+++ clases/Algoritmos.java
@@ -1,50 +1,51 @@
 package clases;
 
-public class Algoritmos{
+public class Algoritmos {
 
-    public static boolean esPar(int numero){
-        return (numero%2) == 0;
+    public static boolean esPar(int numero) {
+        return (numero % 2) == 0;
     }
 
-    //Fermat's Little Theorem
-    public static boolean esPrimo(int numero){
-        if( numero <=  1){
+    // Fermat's Little Theorem
+    public static boolean esPrimo(int numero) {
+        if (numero <= 1) {
             return false;
         }
 
-        for (int i = 2; i < numero/2 ; ++i){
-            if (numero%i == 0) return false;
+        for (int i = 2; i < numero / 2; ++i) {
+            if (numero % i == 0)
+                return false;
         }
 
         return true;
     }
 
-    public static String stringEnReversa(String valor){
+    public static String stringEnReversa(String valor) {
         String nuevoValor = "";
-        for(int i = valor.length()-1; i >= 0; i--){
+        for (int i = valor.length() - 1; i >= 0; i--) {
             nuevoValor += valor.charAt(i);
         }
         return nuevoValor;
     }
 
-    public static boolean esPalindromo(String valor){
+    public static boolean esPalindromo(String valor) {
         valor = valor.replaceAll("\\s", "");
         valor = valor.toLowerCase();
         return valor.compareToIgnoreCase(stringEnReversa(valor)) == 0;
     }
 
-    public static void secuenciaFizzBuzz(int numero){
+    public static void secuenciaFizzBuzz(int numero) {
         String resultado = "";
-        
-        for(int i = 1; i <= numero; i++){
 
-            if( i % 3 == 0){
+        for (int i = 1; i <= numero; i++) {
+
+            if (i % 15 == 0) {
+                resultado += "FizzBuzz ";
+            } else if (i % 3 == 0) {
                 resultado += "Fizz ";
-            }
-            else if(i%5 == 0){
+            } else if (i % 5 == 0) {
                 resultado += "Buzz ";
-            }
-            else{
+            } else {
                 resultado += String.valueOf(i) + " ";
             }
         }
